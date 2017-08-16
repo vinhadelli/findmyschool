418a436179c549ddf98832d2a9e00f84b4d4a2ec
vinhadelli <nathanvinhadelli@gmail.com>
2017-08-16 13:01:53 -0300

    Login Screen and Menu Screen - Temporary

--- app/src/main/java/nathanelucas/findmyschool/LoginActivity.java
+++ app/src/main/java/nathanelucas/findmyschool/LoginActivity.java
@@ -1,7 +1,10 @@
 package nathanelucas.findmyschool;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(this);
+    }
+
+//    @Override
+//    public void onClick(View view)
+//    {
+//        passToMenu(view);
+//    }
+
+    public void passToMenu(View view){
+        startActivity(new Intent(this, MenuActivity.class));
     }
 }
