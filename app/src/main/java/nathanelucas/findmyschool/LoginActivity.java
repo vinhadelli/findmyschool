334c3487634053f906b3a4910c55143f38ca4c7e
lsrios <lsrios01@gmail.com>
2017-09-04 01:38:30 -0300

    Aquele login

--- app/src/main/java/nathanelucas/findmyschool/LoginActivity.java
+++ app/src/main/java/nathanelucas/findmyschool/LoginActivity.java
@@ -1,6 +1,8 @@
 package nathanelucas.findmyschool;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,23 +10,52 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends AppCompatActivity {
+import static android.R.attr.onClick;
 
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
+
+    //variaveis do firebase
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+
+    //variaveis do login google
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleApiClient mGoogleApiClient;
+
+    //Views
+    private final int signEmail = R.id.sign_email;
+    private final int signGoogle = R.id.sign_google;
+    private final int createUserEmail = R.id.create_user_email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mAuth = FirebaseAuth.getInstance();
+        //definindo clickListeners
+        findViewById(signEmail).setOnClickListener(this);
+        findViewById(signGoogle).setOnClickListener(this);
+        findViewById(createUserEmail).setOnClickListener(this);
+
+        //inicializacao do firebase
         mAuthListener = new FirebaseAuth.AuthStateListener(){
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -37,12 +68,29 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         };
+
+        // Configuracao para sign pelo google
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
+
+        if(mAuth.getCurrentUser() != null){
+            //comeca a proxima activity aqui, implementar depois
+        }
     }
 
     @Override
@@ -53,7 +101,8 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    public void createUserFirebase(View view){
+    //metodos do login com email e senha
+    public void createUserFirebase(){
         EditText emailField = (EditText) findViewById(R.id.emailField);
         EditText passwordField = (EditText) findViewById(R.id.passwordField);
         String email = emailField.getText().toString();
@@ -71,7 +120,7 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    public void signInFirebase(View view){
+    public void signInFirebase(){
         EditText emailField = (EditText) findViewById(R.id.emailField);
         EditText passwordField = (EditText) findViewById(R.id.passwordField);
         String email = emailField.getText().toString();
@@ -88,5 +137,87 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    //metodo resultado do chamado da verificacao do google
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // [START_EXCLUDE]
+                //colocar tratamento pra error no login
+                // [END_EXCLUDE]
+            }
+        }
+    }
+
+    //metodo que faz o sign in do google
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("Login pelo Google", "firebaseAuthWithGoogle:" + acct.getId());
+        // [START_EXCLUDE silent]
+//        showProgressDialog(); Vê essa parada aqui depois
+        // [END_EXCLUDE]
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Login G Sucedido", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user); Ver essa parada aqui da resposta depois
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Login g fracasso", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null); tem que dar um jeito nesse bendito metodo
+                        }
+
+                        // [START_EXCLUDE]
+                        //hideProgressDialog(); outro metodo que tem que dar um jeito
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+
+    // Botao de sign Google
+    public void signInGoogle() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("Erro de conexao", "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch(id){
+            case signEmail:
+                signInFirebase();
+                break;
+            case signGoogle:
+                signInGoogle();
+                break;
+            case createUserEmail:
+                createUserFirebase();
+                break;
+        }
     }
 }
